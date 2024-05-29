020400af59010f53851070d61af22d3b3595cb80
vitaly-la <148568747+vitaly-la@users.noreply.github.com>
2024-05-29 16:11:04 +0100

    add salt

--- time_capsule.py
+++ time_capsule.py
@@ -21,20 +21,22 @@ def xor_digest(a: bytes, b: bytes) -> bytes:
 
 
 def generate() -> None:
+    salt = secrets.token_hex()
+
     numbers = [secrets.randbelow(BOUND) for _ in range(PIECES)]
 
     secret = sha256_str(
-        "".join(sha256_str(str(number).encode()) for number in numbers).encode()
+        "".join(sha256_str(f"{salt}{number}".encode()) for number in numbers).encode()
     )
 
-    recovery = []
-    acc = None
-    for number in numbers:
-        number_hash = sha256_bytes(str(number))
-        acc = xor_digest(acc, number_hash) if acc else number_hash
-        recovery.append(sha256_str(acc))
-
     print(f"Secret key: {secret}")
+
+    recovery = []
+    acc = bytes(0 for _ in range(32))
+    for number in numbers:
+        number_hash = sha256_bytes(f"{salt}{number}")
+        acc = xor_digest(acc, number_hash)
+        recovery.append(sha256_str(acc))
 
     ts = datetime.datetime.utcnow().isoformat(timespec="minutes").replace(":", "-")
     secret_path = f"secret-{ts}.txt"
@@ -49,37 +51,41 @@ def generate() -> None:
     print(f"Secret written to {secret_path}")
 
     with open(recovery_path, "w") as recovery_f:
-        recovery_f.write("\n".join([f"{BOUND}"] + recovery + [""]))
+        recovery_f.write("\n".join([str(salt), str(BOUND)] + recovery + [""]))
 
     print(f"Recovery data written to {recovery_path}")
 
 
 def recover(recovery_path: str) -> None:
     with open(recovery_path, "r") as recovery_f:
-        lines = recovery_f.readlines()
+        lines = [line.strip() for line in recovery_f.readlines()]
 
-    bound = int(lines[0].strip())
+    salt = lines[0]
+    bound = int(lines[1])
 
-    acc = None
     number_hashes = []
+    acc = bytes(0 for _ in range(32))
     recovered = 0
     percent = 0
-    for line in lines[1:]:
+    for lineno, line in enumerate(lines[2:]):
         for number in range(bound):
-            number_hash = sha256_bytes(str(number))
-            new_acc = xor_digest(acc, number_hash) if acc else number_hash
+            number_hash = sha256_bytes(f"{salt}{number}")
+            new_acc = xor_digest(acc, number_hash)
 
-            if sha256_str(new_acc) == line.strip():
+            if sha256_str(new_acc) == line:
                 acc = new_acc
-                number_hashes.append(sha256_str(str(number).encode()))
+                number_hashes.append(sha256_str(f"{salt}{number}".encode()))
 
                 recovered += 1
-                new_percent = int(100 * recovered / (len(lines) - 1))
+                new_percent = int(100 * recovered / (len(lines) - 2))
                 if new_percent % 10 == 0 and new_percent > percent:
                     print(f"Recovered {new_percent}%")
                     percent = new_percent
 
                 break
+
+        if sha256_str(acc) != line:
+            raise RuntimeError(f"Recovery file corrupted, line {lineno + 3}")
 
     secret = sha256_str("".join(number_hashes).encode())
     print(f"Secret key: {secret}")
